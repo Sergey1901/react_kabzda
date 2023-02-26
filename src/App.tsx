663875919bb23b505b0d1f109bd9f61b40ee6e1f
Sergey <dor_serg91@mail.ru>
2023-02-26 23:57:10 +0300

    '03-kabzda-solve'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Rating} from "./components/Rating";
+import {Accordion} from "./components/Accordion";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+This is App
+      <Rating value={3} />
+      <Accordion/>
+        <Rating value={5} />
     </div>
   );
 }
+
 
 export default App;
